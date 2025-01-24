60c54d633343f286c6b40150c917b5e7b9ac6aaf
omniroot <omnirootofc@gmail.com>
2025-01-24 21:46:50 +0300

    Update AnimeWatch

--- src/features/animes/components/AnimeWatch/AnimeWatch.tsx
+++ src/features/animes/components/AnimeWatch/AnimeWatch.tsx
@@ -3,9 +3,8 @@ import { HeadingSection } from "@components/ui/HeadingSection/HeadingSection.tsx
 import { useGetAnilibAnime } from "@features/anilib/api/getAnilibAnime/getAnilibAnime.ts";
 import { useGetAnilibEpisodes } from "@features/anilib/api/getAnilibEpisodes/getAnilibEpisodes.api.ts";
 import { IAnime } from "@features/animes/api/anime.interface.ts";
-import { FC, useState } from "react";
+import { FC } from "react";
 import styles from "./AnimeWatch.module.css";
-import { Typography } from "@components/ui/Typography/Typography.tsx";
 
 const getEpisodes = (maxEpisodes: number) => {
 	const elements = Array.from({ length: maxEpisodes }, (_, i) => `${i + 1}`);
@@ -17,16 +16,28 @@ interface IAnimeWatchProps {
 	anime: IAnime;
 }
 export const AnimeWatch: FC<IAnimeWatchProps> = ({ anime }) => {
-	const [episodeElements] = useState(getEpisodes(anime.episodes || anime.episodesAired));
-	const [episodeActive, setEpisodeActive] = useState(episodeElements[0]);
+	// const [episodeElements] = useState(getEpisodes(anime.episodes || anime.episodesAired));
+	// const [episodeActive, setEpisodeActive] = useState(episodeElements[0]);
 	const { data: anilibAnime } = useGetAnilibAnime({ name: anime.name });
 	const { data: episodes, isFetching: episodesLoading } = useGetAnilibEpisodes({
 		variables: { name: anime.name },
 	});
 	// const [anilibEpisodeActive, setAnilibEpisodeActive] = useState(0);
 
+	// const onWatchButtonClick = () => {
+	// 	const anilibEpisode = episodes?.find((ell) => ell.number === episodeActive);
+
+	// 	window.open(
+	// 		`https://anilib.me/ru/anime/${anilibAnime?.[0].slug_url}/watch?episode=${anilibEpisode?.id}`,
+	// 		"_blank",
+	// 	);
+	// 	// getLink();
+	// };
+
 	const onWatchButtonClick = () => {
-		const anilibEpisode = episodes?.find((ell) => ell.number === episodeActive);
+		const anilibEpisode = episodes?.find(
+			(ell) => ell.number === String(anime.userRate.episodes || 1),
+		);
 
 		window.open(
 			`https://anilib.me/ru/anime/${anilibAnime?.[0].slug_url}/watch?episode=${anilibEpisode?.id}`,
@@ -38,10 +49,10 @@ export const AnimeWatch: FC<IAnimeWatchProps> = ({ anime }) => {
 	return (
 		<HeadingSection title="Watch">
 			<div className={styles.watch_content}>
-				<Typography>
+				{/* <Typography>
 					Fix this. After clikc on similar anime and got back episode counts not update!
-				</Typography>
-				<div className={styles.episodes}>
+				</Typography> */}
+				{/* <div className={styles.episodes}>
 					{episodeElements.map((ell) => {
 						return (
 							<button
@@ -53,13 +64,13 @@ export const AnimeWatch: FC<IAnimeWatchProps> = ({ anime }) => {
 							</button>
 						);
 					})}
-				</div>
+				</div> */}
 				<Button
 					className={styles.watch_button}
 					loading={episodesLoading}
 					onClick={onWatchButtonClick}
 				>
-					Watch {episodeActive} episode
+					Watch {anime.userRate.episodes || 1} episode
 				</Button>
 				{/* <Typography>{anilibEpisodeActive}</Typography> */}
 			</div>
