41c111a9c59b224fefc05d635bb3b89680513f3a
omniroot <omnirootofc@gmail.com>
2025-01-27 23:27:27 +0300

    Add millionLint

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import MillionLint from "@million/lint";
+
+export const ReactCompilerConfig = {
+	target: "18", // '17' | '18' | '19'
+};
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		MillionLint.vite({ enabled: true }),
+		react({
+			// babel: {
+			// 		plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+			// 	},
+		}),
+	],
 	resolve: {
 		alias: {
 			"@pages": path.resolve(__dirname, "src", "app", "pages"),
