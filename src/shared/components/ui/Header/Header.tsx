edfe8ca9dcbcaf3445802e5a486b14f2fb20d9ff
omniroot <omnirootofc@gmail.com>
2025-01-28 07:07:58 +0300

    Update Header search button

--- src/shared/components/ui/Header/Header.tsx
+++ src/shared/components/ui/Header/Header.tsx
@@ -1,23 +1,18 @@
 import { SearchIcon } from "@/shared/assets/icons/SearchIcon.tsx";
 import { SettingsIcon } from "@/shared/assets/icons/SettingsIcon.tsx";
+import { Button } from "@components/ui/Button/Button.tsx";
 import { Typography } from "@components/ui/Typography/Typography.tsx";
-import { useSearchStore } from "@features/search/store/search.store.ts";
 import { useHeader } from "@features/storage/stores/header.storage";
+import { Link } from "@tanstack/react-router";
 import { AnimatePresence } from "motion/react";
 import styles from "./Header.module.css";
 
 export const Header = () => {
-	const { toggleOpen } = useSearchStore();
 	const { title, icon } = useHeader();
 	return (
 		<header className={styles.header}>
 			<AnimatePresence mode="popLayout">
-				<div
-					className={styles.left}
-					// initial={{ opacity: 0 }}
-					// animate={{ opacity: 1 }}
-					// exit={{ opacity: 0 }}
-				>
+				<div className={styles.left}>
 					{icon && icon}
 					<Typography size="title" weight="title">
 						{title}
@@ -25,7 +20,11 @@ export const Header = () => {
 				</div>
 			</AnimatePresence>
 			<div className={styles.right}>
-				<SearchIcon onClick={() => toggleOpen()} />
+				<Button variant="ghost" asLink>
+					<Link to="/discovery">
+						<SearchIcon />
+					</Link>
+				</Button>
 				<SettingsIcon />
 			</div>
 		</header>
