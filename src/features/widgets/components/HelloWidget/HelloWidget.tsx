2533c25e61bbe11f091fcc22adf1775993a9bb30
omniroot <omnirootofc@gmail.com>
2025-01-30 00:06:50 +0300

    Add home widget

---
+++ src/features/widgets/components/HelloWidget/HelloWidget.tsx
@@ -0,0 +1,46 @@
+import { CheckListIcon } from "@/shared/assets/icons/CheckListIcon";
+import { BottomSheet } from "@components/ui/BottomSheet/BottomSheet.tsx";
+import { Button } from "@components/ui/Button/Button.tsx";
+import { Typography } from "@components/ui/Typography/Typography.tsx";
+import { useAuth } from "@features/auth/hooks/useAuth.tsx";
+import { useGetUser } from "@features/users/api/getUser/getUser.api.ts";
+import { AnimatePresence } from "motion/react";
+import { useState } from "react";
+import styles from "./HelloWidget.module.css";
+// import { IWidgets, useWidgetsStore } from "@features/widgets/stores/widgets.store.tsx";
+import { Switch } from "@components/ui/Switch/Switch.tsx";
+import { useWidgets, IWidgets } from "@features/widgets/stores/widgets.store.tsx";
+export const HelloWidget = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const { userId } = useAuth();
+	const { data: user } = useGetUser({ variables: { userId: Number(userId) } });
+	const { widgets, toggleWidget } = useWidgets();
+
+	return (
+		<div className={styles.hello_widget}>
+			<Typography size="medium" weight="title">
+				Hello, {user?.nickname || "guest"}!
+			</Typography>
+			<Button variant="background" circle onClick={() => setIsOpen(!isOpen)}>
+				<CheckListIcon />
+			</Button>
+			<AnimatePresence>
+				{isOpen && (
+					<BottomSheet onOutsideClick={() => setIsOpen(!isOpen)} title="Widgets list">
+						<Typography>Widgets list</Typography>
+
+						{Object.entries(widgets).map(([name, state]) => {
+							return (
+								<Switch
+									title={name}
+									state={state}
+									onChange={() => toggleWidget(name as IWidgets)}
+								/>
+							);
+						})}
+					</BottomSheet>
+				)}
+			</AnimatePresence>
+		</div>
+	);
+};
