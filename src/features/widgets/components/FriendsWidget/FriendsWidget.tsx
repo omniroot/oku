e14a68bae795b6fcd2c4255186f759b0d59d153e
omniroot <omnirootofc@gmail.com>
2025-01-30 02:40:31 +0300

    Add Friends widget

---
+++ src/features/widgets/components/FriendsWidget/FriendsWidget.tsx
@@ -0,0 +1,36 @@
+import { Typography } from "@components/ui/Typography/Typography.tsx";
+import styles from "./FriendsWidget.module.css";
+import { useGetUserFriends } from "@features/users/api/getUserFriends/getUserFriends.api.ts";
+import { useAuth } from "@features/auth/hooks/useAuth.tsx";
+import { Loader } from "@components/ui/Loader/Loader.tsx";
+import { ImageView } from "@components/ui/ImageView/ImageView.tsx";
+import { Badge } from "@components/ui/Badge/Badge.tsx";
+import dayjs from "dayjs";
+
+export const FriendsWidget = () => {
+	const { userId } = useAuth();
+	const { data: friends, isLoading } = useGetUserFriends({ variables: { userId: Number(userId) } });
+
+	if (isLoading) return <Loader />;
+	if (!friends) return null;
+	return (
+		<div className={styles.friends_widget}>
+			{friends.map((friend) => {
+				return (
+					<div className={styles.friend}>
+						<ImageView src={friend.image.x160} className={styles.avatar} />
+						<div className={styles.info}>
+							<div className={styles.line}>
+								<Typography size="base" weight="title">
+									{friend.nickname}
+								</Typography>
+								<Badge className={styles.badge}>{dayjs(friend.last_online_at).fromNow()}</Badge>
+							</div>
+							{/* <div className={styles.line}></div> */}
+						</div>
+					</div>
+				);
+			})}
+		</div>
+	);
+};
--- src/features/widgets/stores/widgets.store.tsx
+++ src/features/widgets/stores/widgets.store.tsx
@@ -1,16 +1,19 @@
+import { FriendsWidget } from "@features/widgets/components/FriendsWidget/FriendsWidget.tsx";
 import { HelloWidget } from "@features/widgets/components/HelloWidget/HelloWidget.tsx";
 import { TestWidget } from "@features/widgets/components/TestWidget/TestWidget.tsx";
 import { useAtom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 
-export type IWidgets = "hello" | "test";
+export type IWidgets = "hello" | "friends" | "test";
 
 export const WidgetComponents: Record<IWidgets, React.ReactNode> = {
 	hello: <HelloWidget />,
+	friends: <FriendsWidget />,
 	test: <TestWidget />,
 };
 
 interface IWidgetsStore {
+	version: number;
 	widgets: Record<IWidgets, boolean>;
 	getWidget: (name: IWidgets) => boolean;
 	toggleWidget: (name: IWidgets) => void;
@@ -18,6 +21,7 @@ interface IWidgetsStore {
 
 const widgetsStore = atomWithStorage<IWidgetsStore["widgets"]>("widgets", {
 	hello: true,
+	friends: false,
 	test: true,
 });
 
