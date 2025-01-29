ce8c59680c3620b815ee7d6b33391ad822e14967
omniroot <omnirootofc@gmail.com>
2025-01-30 00:07:28 +0300

    Use widgets in home page

--- src/app/pages/home/home.page.tsx
+++ src/app/pages/home/home.page.tsx
@@ -1,10 +1,19 @@
-import { HelloWidget } from "@features/widgets/HelloWidget/HelloWidget.tsx";
 import styles from "./home.page.module.css";
+import { useHeader } from "@features/storage/stores/header.storage.ts";
+import { IWidgets, useWidgets, WidgetComponents } from "@features/widgets/stores/widgets.store.tsx";
+import { useEffect } from "react";
 
 export const HomePage = () => {
+	const { setTitle } = useHeader();
+	const { widgets } = useWidgets();
+	console.log({ widgets });
+
+	useEffect(() => {
+		setTitle("Home");
+	}, [setTitle]);
 	return (
 		<div className={styles.page}>
-			<HelloWidget />
+			{Object.entries(widgets).map(([name, state]) => state && WidgetComponents[name as IWidgets])}
 		</div>
 	);
 };
