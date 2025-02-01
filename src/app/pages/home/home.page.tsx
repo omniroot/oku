5d511362119e61e76bee7e4203a5166b7f7e12a9
omniroot <omnirootofc@gmail.com>
2025-02-01 12:28:35 +0300

    Test BottomSheet in home page

--- src/app/pages/home/home.page.tsx
+++ src/app/pages/home/home.page.tsx
@@ -1,9 +1,12 @@
-import styles from "./home.page.module.css";
+import { BottomSheet } from "@components/ui/BottomSheet/BottomSheet.tsx";
+import { Button } from "@components/ui/Button/Button.tsx";
 import { useHeader } from "@features/storage/stores/header.storage.ts";
 import { IWidgets, useWidgets, WidgetComponents } from "@features/widgets/stores/widgets.store.tsx";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import styles from "./home.page.module.css";
 
 export const HomePage = () => {
+	const [show, setShow] = useState(false);
 	const { setTitle } = useHeader();
 	const { widgets } = useWidgets();
 	console.log({ widgets });
@@ -11,9 +14,16 @@ export const HomePage = () => {
 	useEffect(() => {
 		setTitle("Home");
 	}, [setTitle]);
+
 	return (
 		<div className={styles.page}>
 			{Object.entries(widgets).map(([name, state]) => state && WidgetComponents[name as IWidgets])}
+			<Button onClick={() => setShow((prev) => !prev)}>Show</Button>
+
+			<BottomSheet title="Bottom sheet" isShow={show} onOutsideClick={() => setShow(false)}>
+				<span>123</span>
+				<span>456</span>
+			</BottomSheet>
 		</div>
 	);
 };
