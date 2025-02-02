3fbd014b8ee181a8193c5f3247bedf1b7f96cc73
omniroot <omnirootofc@gmail.com>
2025-02-02 14:51:43 +0300

    Add right slot in Switch

--- src/shared/components/ui/Switch/Switch.tsx
+++ src/shared/components/ui/Switch/Switch.tsx
@@ -1,14 +1,17 @@
-import { FC, useState } from "react";
+import { FC, ReactNode, useState } from "react";
 import styles from "./Switch.module.css";
 import { Typography } from "@components/ui/Typography/Typography.tsx";
 import { motion } from "motion/react";
+
 interface ISwitchProps {
 	title: string;
 	state: boolean;
 	onChange?: (state: boolean) => void;
 	disabled?: boolean;
+	rightSlot?: ReactNode;
 }
-export const Switch: FC<ISwitchProps> = ({ title, state, onChange, disabled }) => {
+
+export const Switch: FC<ISwitchProps> = ({ title, state, onChange, disabled, rightSlot }) => {
 	const [enabled, setEnabled] = useState(state);
 
 	const toggleEnabled = () => {
@@ -21,23 +24,26 @@ export const Switch: FC<ISwitchProps> = ({ title, state, onChange, disabled }) =
 	return (
 		<div className={styles.switch}>
 			<Typography size="medium">{title}</Typography>
-			<motion.div
-				className={styles.checkbox}
-				style={{ justifyContent: enabled ? "flex-end" : "flex-start" }}
-				onClick={toggleEnabled}
-				data-disabled={disabled}
-			>
+			<div className={styles.right}>
+				{rightSlot}
 				<motion.div
-					className={styles.state}
-					data-enabled={enabled}
-					layout
-					initial={{ scale: 0.5 }}
-					animate={{ scale: enabled ? 1 : 0.9 }}
-					exit={{ scale: 0.5 }}
-					transition={{ duration: 0.2 }}
+					className={styles.checkbox}
+					style={{ justifyContent: enabled ? "flex-end" : "flex-start" }}
+					onClick={toggleEnabled}
 					data-disabled={disabled}
-				></motion.div>
-			</motion.div>
+				>
+					<motion.div
+						className={styles.state}
+						data-enabled={enabled}
+						layout
+						initial={{ scale: 0.5 }}
+						animate={{ scale: enabled ? 1 : 0.9 }}
+						exit={{ scale: 0.5 }}
+						transition={{ duration: 0.2 }}
+						data-disabled={disabled}
+					></motion.div>
+				</motion.div>
+			</div>
 		</div>
 	);
 };
