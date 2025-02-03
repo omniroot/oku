ae1457ca4a4fcd283d8ec33069d4f5eb9359a013
omniroot <omnirootofc@gmail.com>
2025-02-03 20:07:42 +0300

    Remove ui components

--- src/shared/components/ui/ButtonGroup/ButtonGroup.utils.tsx
+++
@@ -1,5 +0,0 @@
-import { IButtonGroupElement } from "@ui/ButtonGroup/ButtonGroup.tsx";
-
-export const getButtonGroupElementById = (list: IButtonGroupElement[], id: string) => {
-	return list.find((filter) => filter.id === id) || list[0];
-};
