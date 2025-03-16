83902b69daf5b73a069241e20d46ec0cb96a1000
omniroot <omnirootofc@gmail.com>
2025-03-16 13:16:22 +0300

    Add CheckIcon

---
+++ src/shared/assets/icons/CheckIcon.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+export const CheckIcon = (props: React.SVGProps<SVGSVGElement>) => {
+	return (
+		<svg
+			width="800px"
+			height="800px"
+			viewBox="0 0 24 24"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+			{...props}
+		>
+			<path
+				d="M4 12.6111L8.92308 17.5L20 6.5"
+				stroke="currentColor"
+				strokeWidth={2}
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+		</svg>
+	);
+};
