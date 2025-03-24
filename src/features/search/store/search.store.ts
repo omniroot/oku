49c35deefa61b063aa706358e66a9509d757c361
omniroot <omnirootofc@gmail.com>
2025-03-24 15:22:51 +0300

    Update search.store.ts

--- src/features/search/store/search.store.ts
+++ src/features/search/store/search.store.ts
@@ -1,12 +1,17 @@
+import { IAnimeStatus } from "@features/animes/api/getAnimes/getAnimes.types.ts";
 import { useAtom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 
 interface ISearch {
 	query: string;
+	type: "anime" | "hentai" | "user" | "manga";
+	status: IAnimeStatus;
 }
 
 const searchStore = atomWithStorage<ISearch>("search", {
 	query: "",
+	status: "",
+	type: "anime",
 });
 
 export const useSearch = () => {
