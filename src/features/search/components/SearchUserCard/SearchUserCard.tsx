ca1e3b055d77942a7ea303de6cd4625572639cab
omniroot <omnirootofc@gmail.com>
2025-04-06 15:52:56 +0300

    Add SearchUserCard

---
+++ src/features/search/components/SearchUserCard/SearchUserCard.tsx
@@ -0,0 +1,27 @@
+import { IGetUser } from "@features/users/api/getUsers/getUsers.api.ts";
+import { FC } from "react";
+import styles from "./SearchUserCard.module.css";
+import { Typography } from "@components/ui/Typography/Typography.tsx";
+import { ImageView } from "@components/ui/ImageView/ImageView.tsx";
+import { Badge } from "@components/ui/Badge/Badge.tsx";
+import dayjs from "dayjs";
+import { Link } from "@tanstack/react-router";
+
+interface IProps {
+	user: IGetUser;
+}
+
+export const SearchUserCard: FC<IProps> = ({ user }) => {
+	const lastOnline = dayjs(user.lastOnlineAt).fromNow();
+	return (
+		<Link to="/users/$userId" params={{ userId: user.id }} className={styles.search_user_card}>
+			<ImageView src={user.avatarUrl || "404.png"} className={styles.avatar} />
+			<div>
+				<Typography size="medium" weight="title">
+					{user.nickname}
+				</Typography>
+				<Badge>{lastOnline}</Badge>
+			</div>
+		</Link>
+	);
+};
