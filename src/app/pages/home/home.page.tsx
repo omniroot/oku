a344ffc3cb510fd230d56a16af988cce8adec6b4
omniroot <omnirootofc@gmail.com>
2025-04-06 15:53:46 +0300

    Add some tests in home page

--- src/app/pages/home/home.page.tsx
+++ src/app/pages/home/home.page.tsx
@@ -1,5 +1,5 @@
 import { useHeader } from "@features/storage/stores/header.storage.ts";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import styles from "./home.page.module.css";
 import { WidgetsList } from "@features/widgets/components/WidgetsList/WidgetsList.tsx";
 import { useNotifications } from "@features/notifications/stores/notifications.store.tsx";
@@ -9,16 +9,37 @@ export const HomePage = () => {
 	const { setTitle } = useHeader();
 	const { addNotification } = useNotifications();
 
+	const not = useCallback(() => {
+		addNotification({ message: `HI ${Math.random()}` });
+	}, []);
+
 	useEffect(() => {
 		setTitle("Home");
 	}, [setTitle]);
 
 	return (
 		<div className={styles.page}>
-			<WidgetsList />
-			<Button onClick={() => addNotification({ message: `HI ${Math.random()}` })}>
-				Add notificaition
-			</Button>
+			{/* <WidgetsList /> */}
+			<Button onClick={not}>Add notificaition</Button>
+
+			<div className={styles.block} style={{ backgroundColor: "var(--surface_container_lowest)" }}>
+				surface_container_lowest
+			</div>
+			<div className={styles.block} style={{ backgroundColor: "var(--surface)" }}>
+				surface
+			</div>
+			<div className={styles.block} style={{ backgroundColor: "var(--surface_container_low)" }}>
+				surface_container_low
+			</div>
+			<div className={styles.block} style={{ backgroundColor: "var(--surface_container)" }}>
+				surface_container
+			</div>
+			<div className={styles.block} style={{ backgroundColor: "var(--surface_container_high)" }}>
+				surface_container_high
+			</div>
+			<div className={styles.block} style={{ backgroundColor: "var(--surface_container_highest)" }}>
+				surface_container_highest
+			</div>
 		</div>
 	);
 };
