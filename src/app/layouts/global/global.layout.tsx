0649e04fa3f6bc32e554079f7a15ff6033315a8d
omniroot <omnirootofc@gmail.com>
2025-04-07 11:00:03 +0300

    Add Global fab layout and fab buttons

--- src/app/layouts/global/global.layout.tsx
+++ src/app/layouts/global/global.layout.tsx
@@ -1,6 +1,11 @@
 import { NotificationsLayout } from "@/app/layouts/notifications/notifications.layout.tsx";
 import { SearchLayout } from "@/app/layouts/search/search.layout.tsx";
+import { HeartIcon } from "@/shared/assets/icons/HeartIcon.tsx";
+import { HomeIcon } from "@/shared/assets/icons/HomeIcon.tsx";
+import { TrashIcon } from "@/shared/assets/icons/TrashIcon.tsx";
 import { ModernBottomNavigation } from "@components/business/ModernBottomNavigation/ModernBottomNavigation.tsx";
+import { FAB } from "@components/ui/FAB/FAB.tsx";
+import { FABLayout } from "@components/ui/FABLayout/FABLayout.tsx";
 import { Header } from "@components/ui/Header/Header.tsx";
 import { Outlet } from "@tanstack/react-router";
 
@@ -15,6 +20,16 @@ export const GlobalLayout = () => {
 			<NotificationsLayout />
 			<ModernBottomNavigation />
 			<SearchLayout />
+			<FABLayout>
+				<FAB variant="secondary" size="small">
+					<HeartIcon />
+					heart
+				</FAB>
+				<FAB variant="primary">
+					<HomeIcon />
+					whefhweifoheow
+				</FAB>
+			</FABLayout>
 		</>
 	);
 };
