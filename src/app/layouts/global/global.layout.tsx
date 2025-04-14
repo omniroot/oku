41e120e6f24eea32e4b71941cb210bbfb6cd696e
omniroot <omnirootofc@gmail.com>
2025-04-14 17:56:37 +0300

    Use PageTransition in global layout

--- src/app/layouts/global/global.layout.tsx
+++ src/app/layouts/global/global.layout.tsx
@@ -2,11 +2,12 @@ import { NotificationsLayout } from "@/app/layouts/notifications/notifications.l
 import { SearchLayout } from "@/app/layouts/search/search.layout.tsx";
 import { HeartIcon } from "@/shared/assets/icons/HeartIcon.tsx";
 import { HomeIcon } from "@/shared/assets/icons/HomeIcon.tsx";
-import { TrashIcon } from "@/shared/assets/icons/TrashIcon.tsx";
+import "@/shared/components/ui/UIProvider/UIProvider.module.css";
 import { ModernBottomNavigation } from "@components/business/ModernBottomNavigation/ModernBottomNavigation.tsx";
 import { FAB } from "@components/ui/FAB/FAB.tsx";
 import { FABLayout } from "@components/ui/FABLayout/FABLayout.tsx";
 import { Header } from "@components/ui/Header/Header.tsx";
+import { PageTransition } from "@components/ui/PageTransition/PageTransition.tsx";
 import { Outlet } from "@tanstack/react-router";
 
 export const GlobalLayout = () => {
@@ -14,7 +15,9 @@ export const GlobalLayout = () => {
 		<>
 			<Header />
 			<main>
-				<Outlet />
+				<PageTransition>
+					<Outlet />
+				</PageTransition>
 			</main>
 			{/* <BottomNavigation /> */}
 			<NotificationsLayout />
