f98b12d98c16dc27379af673684e61b050c6cddc
omniroot <omnirootofc@gmail.com>
2025-04-14 17:51:57 +0300

    Update settings page, add more preview

--- src/app/pages/settings/settings.page.tsx
+++ src/app/pages/settings/settings.page.tsx
@@ -1,19 +1,28 @@
-// import { Button } from "@ui/Button/Button.tsx";
-// import { Divider } from "@ui/Divider/Divider.tsx";
-// import { HeadingSection } from "@ui/HeadingSection/HeadingSection.tsx";
 import { Typography } from "@components/ui/Typography/Typography.tsx";
 import styles from "./settings.page.module.css";
-// import { createLazyRoute } from "@tanstack/react-router";
 
-import { createLazyRoute } from "@tanstack/react-router";
+import { Accordion } from "@components/ui/Accordion/Accordion.tsx";
+import { Box } from "@components/ui/Box/Box.tsx";
 import { Button } from "@components/ui/Button/Button.tsx";
 import { Divider } from "@components/ui/Divider/Divider.tsx";
 import { HeadingSection } from "@components/ui/HeadingSection/HeadingSection.tsx";
+import { Input } from "@components/ui/Input/Input.tsx";
+import { createLazyRoute } from "@tanstack/react-router";
+import { useEffect, useState } from "react";
+import { useMaterialTheme } from "@/shared/MaterialTheme/MaterialTheme.tsx";
+import { useHeader } from "@features/storage/stores/header.storage.ts";
 
 export const SettingsPage = () => {
+	const { color, changeColor } = useMaterialTheme();
+	const { setTitle } = useHeader();
+	const [newColor, setNewColor] = useState(color);
+
+	useEffect(() => {
+		setTitle("Settings");
+	}, [setTitle]);
+
 	return (
 		<div className={styles.page}>
-			<Typography>Settings page</Typography>
 			<HeadingSection title="Preview theme">
 				<div className={styles.preview_colors_content}>
 					<div className={styles.preview_colors}>
@@ -32,16 +41,106 @@ export const SettingsPage = () => {
 						</section>
 					</div>
 					<Divider spacing />
-					<Button variant="primary">Primary Button</Button>
-					<Button variant="secondary">Secondary Button</Button>
-					<Button variant="tertiary">Tertiary Button</Button>
-					<Button variant="outline">Outline Button</Button>
-					<Button variant="outline" loading>
-						Outline Button
-					</Button>
-					<Button variant="gradient">Gradient Button</Button>
-					<Button variant="ghost">Ghost Button</Button>
-					<Button variant="delete">Delete Button</Button>
+					<Input value={newColor} onChange={setNewColor} onSubmit={() => changeColor(newColor)} />
+					<Accordion title="Colors">
+						<Box color="primary" backgroundColor="primary">
+							primary
+						</Box>
+						<Box color="primary_container" backgroundColor="primary_container">
+							primary container
+						</Box>
+						<Box color="secondary" backgroundColor="secondary">
+							secondary
+						</Box>
+						<Box color="secondary_container" backgroundColor="secondary_container">
+							secondary container
+						</Box>
+						<Box color="tertiary" backgroundColor="tertiary">
+							tertiary
+						</Box>
+						<Box color="tertiary_container" backgroundColor="tertiary_container">
+							tertiary container
+						</Box>
+						<Box color="error" backgroundColor="error">
+							error
+						</Box>
+						<Box color="error_container" backgroundColor="error_container">
+							error container
+						</Box>
+						<Box color="surface" backgroundColor="surface">
+							surface
+						</Box>
+						<Box color="surface_container" backgroundColor="surface_container">
+							surface container
+						</Box>
+						<Box color="surface_container_high" backgroundColor="surface_container_high">
+							surface container high
+						</Box>
+					</Accordion>
+					<Accordion title="Buttons">
+						<Button variant="primary">Primary Button</Button>
+						<Button variant="secondary">Secondary Button</Button>
+						<Button variant="tertiary">Tertiary Button</Button>
+						<Button variant="outline">Outline Button</Button>
+						<Button variant="outline" loading>
+							Outline Button
+						</Button>
+						<Button variant="gradient">Gradient Button</Button>
+						<Button variant="ghost">Ghost Button</Button>
+						<Button variant="delete">Delete Button</Button>
+						<Button variant="error">Error Button</Button>
+					</Accordion>
+					<Accordion title="Typography">
+						<Typography variant="display" size="large">
+							display large
+						</Typography>
+						<Typography variant="display" size="medium">
+							display medium
+						</Typography>
+						<Typography variant="display" size="small">
+							display small
+						</Typography>
+						<Divider spacing />
+						<Typography variant="headline" size="large">
+							headline large
+						</Typography>
+						<Typography variant="headline" size="medium">
+							headline medium
+						</Typography>
+						<Typography variant="headline" size="small">
+							headline small
+						</Typography>
+						<Divider spacing />
+						<Typography variant="title" size="large">
+							title large
+						</Typography>
+						<Typography variant="title" size="medium">
+							title medium
+						</Typography>
+						<Typography variant="title" size="small">
+							title small
+						</Typography>
+						<Divider spacing />
+						<Typography variant="body" size="large">
+							body large
+						</Typography>
+						<Typography variant="body" size="medium">
+							body medium
+						</Typography>
+						<Typography variant="body" size="small">
+							body small
+						</Typography>
+						<Divider spacing />
+						<Typography variant="label" size="large">
+							label large
+						</Typography>
+						<Typography variant="label" size="medium">
+							label medium
+						</Typography>
+						<Typography variant="label" size="small">
+							label small
+						</Typography>
+					</Accordion>
 				</div>
 			</HeadingSection>
 		</div>
