1f30c53f43640d485872a1e47d0248a8ba8451dd
omniroot <omnirootofc@gmail.com>
2025-04-25 03:05:27 +0300

    Move some debug previews from home page to settings page

--- src/app/pages/home/home.page.tsx
+++ src/app/pages/home/home.page.tsx
@@ -1,18 +1,10 @@
-import { Accordion } from "@components/ui/Accordion/Accordion.tsx";
-import { Button } from "@components/ui/Button/Button.tsx";
-import { Typography } from "@components/ui/Typography/Typography.tsx";
-import { useNotifications } from "@features/notifications/stores/notifications.store.tsx";
 import { useHeader } from "@features/storage/stores/header.storage.ts";
-import { useCallback, useEffect } from "react";
+import { useEffect } from "react";
 import styles from "./home.page.module.css";
+import { WidgetsList } from "@features/widgets/components/WidgetsList/WidgetsList.tsx";
 
 export const HomePage = () => {
 	const { setTitle } = useHeader();
-	const { addNotification } = useNotifications();
-
-	const not = useCallback(() => {
-		addNotification({ message: `HI ${Math.random()}` });
-	}, [addNotification]);
 
 	useEffect(() => {
 		setTitle("Home");
@@ -20,63 +12,7 @@ export const HomePage = () => {
 
 	return (
 		<div className={styles.page}>
-			{/* <WidgetsList /> */}
-			<Accordion title="Accrodion">
-				<Typography variant="label" size="large">
-					label size large
-				</Typography>
-				<Typography variant="label" size="medium">
-					label size medium
-				</Typography>
-				<Typography variant="label" size="small">
-					label size small
-				</Typography>
-				<div className={styles.block} style={{ backgroundColor: "var(--tertiary)" }}>
-					<Typography variant="headline" color="tertiary">
-						Typography
-					</Typography>
-				</div>
-				{/* <div className={styles.block} style={{ backgroundColor: getMd("primary") }}>
-					<span style={{ color: getMd("on-primary") }}>new</span>
-				</div> */}
-			</Accordion>
-
-			<Button onClick={not}>Add notificaition</Button>
-			<Button onClick={not} style={{ backgroundColor: "var(--color-primary)" }}>
-				Add notificaition
-			</Button>
-
-			<div className={styles.block} style={{ backgroundColor: "var(--surface_container_lowest)" }}>
-				surface_container_lowest
-			</div>
-			<div className={styles.block} style={{ backgroundColor: "var(--surface)" }}>
-				<Typography color="surface">surface</Typography>
-			</div>
-			<div className={styles.block} style={{ backgroundColor: "var(--surface_container_low)" }}>
-				<Typography color="surface">surface_container_low</Typography>
-			</div>
-			<div className={styles.block} style={{ backgroundColor: "var(--surface_container)" }}>
-				surface_container
-			</div>
-			<div className={styles.block} style={{ backgroundColor: "var(--surface_container_high)" }}>
-				surface_container_high
-			</div>
-			<div className={styles.block} style={{ backgroundColor: "var(--surface_container_highest)" }}>
-				surface_container_highest
-			</div>
-			<div className={styles.block} style={{ backgroundColor: "var(--secondary)" }}>
-				<Typography color="secondary">hello world</Typography>
-			</div>
-
-			<div className={styles.block} style={{ backgroundColor: "var(--tertiary)" }}>
-				<Typography color="tertiary">hello world</Typography>
-			</div>
-			<div className={styles.block} style={{ backgroundColor: "var(--error)" }}>
-				<Typography color="error">hello world</Typography>
-			</div>
-			<div className={styles.block} style={{ backgroundColor: "var(--error_container)" }}>
-				<Typography color="error_container">hello world</Typography>
-			</div>
+			<WidgetsList />
 		</div>
 	);
 };
--- src/app/pages/settings/settings.page.tsx
+++ src/app/pages/settings/settings.page.tsx
@@ -8,14 +8,21 @@ import { Divider } from "@components/ui/Divider/Divider.tsx";
 import { HeadingSection } from "@components/ui/HeadingSection/HeadingSection.tsx";
 import { Input } from "@components/ui/Input/Input.tsx";
 import { createLazyRoute } from "@tanstack/react-router";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useMaterialTheme } from "@/shared/MaterialTheme/MaterialTheme.tsx";
 import { useHeader } from "@features/storage/stores/header.storage.ts";
+import { useNotifications } from "@features/notifications/stores/notifications.store.tsx";
 
 export const SettingsPage = () => {
 	const { color, changeColor } = useMaterialTheme();
 	const { setTitle } = useHeader();
 	const [newColor, setNewColor] = useState(color);
+
+	const { addNotification } = useNotifications();
+
+	const not = useCallback(() => {
+		addNotification({ message: `HI ${Math.random()}` });
+	}, [addNotification]);
 
 	useEffect(() => {
 		setTitle("Settings");
@@ -141,6 +148,68 @@ export const SettingsPage = () => {
 							label small
 						</Typography>
 					</Accordion>
+				</div>
+				<Accordion title="Accrodion">
+					<Typography variant="label" size="large">
+						label size large
+					</Typography>
+					<Typography variant="label" size="medium">
+						label size medium
+					</Typography>
+					<Typography variant="label" size="small">
+						label size small
+					</Typography>
+					<div className={styles.block} style={{ backgroundColor: "var(--tertiary)" }}>
+						<Typography variant="headline" color="tertiary">
+							Typography
+						</Typography>
+					</div>
+					{/* <div className={styles.block} style={{ backgroundColor: getMd("primary") }}>
+					<span style={{ color: getMd("on-primary") }}>new</span>
+				</div> */}
+				</Accordion>
+
+				<Button onClick={not}>Add notificaition</Button>
+				<Button onClick={not} style={{ backgroundColor: "var(--color-primary)" }}>
+					Add notificaition
+				</Button>
+
+				<div
+					className={styles.block}
+					style={{ backgroundColor: "var(--surface_container_lowest)" }}
+				>
+					surface_container_lowest
+				</div>
+				<div className={styles.block} style={{ backgroundColor: "var(--surface)" }}>
+					<Typography color="surface">surface</Typography>
+				</div>
+				<div className={styles.block} style={{ backgroundColor: "var(--surface_container_low)" }}>
+					<Typography color="surface">surface_container_low</Typography>
+				</div>
+				<div className={styles.block} style={{ backgroundColor: "var(--surface_container)" }}>
+					surface_container
+				</div>
+				<div className={styles.block} style={{ backgroundColor: "var(--surface_container_high)" }}>
+					surface_container_high
+				</div>
+				<div
+					className={styles.block}
+					style={{ backgroundColor: "var(--surface_container_highest)" }}
+				>
+					surface_container_highest
+				</div>
+				<div className={styles.block} style={{ backgroundColor: "var(--secondary)" }}>
+					<Typography color="secondary">hello world</Typography>
+				</div>
+
+				<div className={styles.block} style={{ backgroundColor: "var(--tertiary)" }}>
+					<Typography color="tertiary">hello world</Typography>
+				</div>
+				<div className={styles.block} style={{ backgroundColor: "var(--error)" }}>
+					<Typography color="error">hello world</Typography>
+				</div>
+				<div className={styles.block} style={{ backgroundColor: "var(--error_container)" }}>
+					<Typography color="error_container">hello world</Typography>
 				</div>
 			</HeadingSection>
 		</div>
