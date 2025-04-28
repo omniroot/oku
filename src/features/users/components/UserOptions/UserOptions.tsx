0d462ba3f002a2a1760157361deccc7a1cd13aab
omniroot <omnirootofc@gmail.com>
2025-04-29 00:45:19 +0300

    Use ListView in UserOption

--- src/features/users/components/UserOptions/UserOptions.tsx
+++ src/features/users/components/UserOptions/UserOptions.tsx
@@ -1,48 +1,38 @@
 import { CupIcon } from "@/shared/assets/icons/CupIcon.tsx";
 import { HeartIcon } from "@/shared/assets/icons/HeartIcon.tsx";
 import { UsersIcon } from "@/shared/assets/icons/UsersIcon.tsx";
+import { ListView } from "@components/ui/ListView/ListView.tsx";
 import { Typography } from "@components/ui/Typography/Typography.tsx";
-import { IUser } from "@features/users/api/getUser/getUser.types.ts";
 import { Link } from "@tanstack/react-router";
 import { FC } from "react";
 import styles from "./UserOptions.module.css";
 
 interface IUserOptionsProps {
-	user: IUser;
+	userId: string;
 }
-export const UserOptions: FC<IUserOptionsProps> = ({ user }) => {
+export const UserOptions: FC<IUserOptionsProps> = ({ userId }) => {
 	return (
 		<div className={styles.user_options}>
-			<Link
-				className={styles.option}
-				to="/users/$userId/friends"
-				params={{ userId: String(user.id) }}
-			>
-				<UsersIcon />
-				<Typography variant="body" size="large">
-					friends
-				</Typography>
-			</Link>
-			<Link
-				className={styles.option}
-				to="/users/$userId/favorites"
-				params={{ userId: String(user.id) }}
-			>
-				<HeartIcon />
-				<Typography variant="body" size="large">
-					favorites
-				</Typography>
-			</Link>
-			<Link
-				className={styles.option}
-				to="/users/$userId/achievements"
-				params={{ userId: String(user.id) }}
-			>
-				<CupIcon />
-				<Typography variant="body" size="large">
-					achievements
-				</Typography>
-			</Link>
+			<ListView>
+				<Link className={styles.option} to="/users/$userId/friends" params={{ userId }}>
+					<UsersIcon />
+					<Typography variant="body" size="large">
+						friends
+					</Typography>
+				</Link>
+				<Link className={styles.option} to="/users/$userId/favorites" params={{ userId }}>
+					<HeartIcon />
+					<Typography variant="body" size="large">
+						favorites
+					</Typography>
+				</Link>
+				<Link className={styles.option} to="/users/$userId/achievements" params={{ userId }}>
+					<CupIcon />
+					<Typography variant="body" size="large">
+						achievements
+					</Typography>
+				</Link>
+			</ListView>
 		</div>
 	);
 };
