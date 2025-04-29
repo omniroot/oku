34e3dac69032e00a1d27f39b247279419f973d1f
omniroot <omnirootofc@gmail.com>
2025-04-29 16:13:53 +0300

    Add mini variant in UserItem

--- src/features/users/components/UserItem/UserItem.tsx
+++ src/features/users/components/UserItem/UserItem.tsx
@@ -1,34 +1,42 @@
+import { Badge } from "@components/ui/Badge/Badge.tsx";
+import { ImageView } from "@components/ui/ImageView/ImageView.tsx";
+import { Tooltip } from "@components/ui/Tooltip/Tooltip.tsx";
+import { Typography } from "@components/ui/Typography/Typography.tsx";
 import { Link } from "@tanstack/react-router";
+import dayjs from "dayjs";
 import { FC } from "react";
 import styles from "./UserItem.module.css";
-import { ImageView } from "@components/ui/ImageView/ImageView.tsx";
-import { Typography } from "@components/ui/Typography/Typography.tsx";
-import { Badge } from "@components/ui/Badge/Badge.tsx";
-import dayjs from "dayjs";
-import { Tooltip } from "@components/ui/Tooltip/Tooltip.tsx";
 
 interface IProps {
 	id: string;
 	avatar?: string;
 	title?: string;
 	lastOnline?: string;
+	variant?: "mini" | "normal";
 }
 
-export const UserItem: FC<IProps> = ({ id, avatar, title, lastOnline }) => {
+export const UserItem: FC<IProps> = ({ id, avatar, title, lastOnline, variant = "normal" }) => {
 	const _lastOnline = dayjs(lastOnline).fromNow();
 	const _userOnline = lastOnline === "сейчас на сайте" ? "Online" : "Offline";
 
 	return (
-		<Link to="/users/$userId" params={{ userId: id }} className={styles.useritem}>
+		<Link
+			to="/users/$userId"
+			params={{ userId: id }}
+			className={styles.useritem}
+			data-variant={variant}
+		>
 			<ImageView src={avatar || ""} className={styles.avatar} />
-			{/* <div className={styles.info}> */}
-			<Typography variant="title" size="large">
+			<Typography variant="title" size={variant === "mini" ? "medium" : "large"}>
 				{title}
 			</Typography>
-			<Tooltip title={_lastOnline}>
+
+			<Tooltip
+				title={_lastOnline}
+				style={variant === "mini" ? { position: "absolute", right: 10, bottom: 40 } : {}}
+			>
 				<Badge>{_userOnline}</Badge>
 			</Tooltip>
-			{/* </div> */}
 		</Link>
 	);
 };
