3731ade499eed8c79cbfec8ccd601ed4374d4993
omniroot <omnirootofc@gmail.com>
2025-04-30 03:02:08 +0300

    Change AnimeMoreInfo style

--- src/features/animes/components/AnimeMoreInfo/AnimeMoreInfo.tsx
+++ src/features/animes/components/AnimeMoreInfo/AnimeMoreInfo.tsx
@@ -1,17 +1,42 @@
-import styles from "./AnimeMoreInfo.module.css";
-import { FC } from "react";
-import { motion } from "motion/react";
+import { ListView } from "@components/ui/ListView/ListView.tsx";
 import { IAnime } from "@features/animes/api/anime.interface.ts";
+import { FC } from "react";
+import styles from "./AnimeMoreInfo.module.css";
+
+const getColorByScore = (score: number) => {
+	const colorMap = {
+		"очень плохо": "#FFCCCC", // Красный
+		плохо: "#FF9999", // Светло-красный
+		нормально: "#FFFF99", // Желтый
+		хорошо: "#99FF99", // Светло-зеленый
+		отлично: "#99FFCC", // Очень светло-зеленый
+	};
+
+	// Определяем цвет в зависимости от оценки
+	if (score < 3) {
+		return colorMap["очень плохо"];
+	} else if (score < 5) {
+		return colorMap["плохо"];
+	} else if (score < 7) {
+		return colorMap["нормально"];
+	} else if (score < 9) {
+		return colorMap["хорошо"];
+	} else {
+		return colorMap["отлично"];
+	}
+};
 
 interface IAnimeMoreInfoProps {
 	anime: IAnime;
 }
 export const AnimeMoreInfo: FC<IAnimeMoreInfoProps> = ({ anime }) => {
 	return (
-		<motion.div className={styles.anime_more_info} initial={{ scale: 0.9 }} animate={{ scale: 1 }}>
+		<ListView orientation="horizontal" className={styles.anime_more_info}>
 			<div className={styles.element}>
 				<span className={styles.title}>Skikimiori</span>
-				<span className={styles.content}>{anime.score}</span>
+				<span className={styles.content} style={{ color: getColorByScore(anime.score) }}>
+					{anime.score}
+				</span>
 			</div>
 			<div className={styles.element}>
 				<span className={styles.title}>Episodes</span>
@@ -21,6 +46,6 @@ export const AnimeMoreInfo: FC<IAnimeMoreInfoProps> = ({ anime }) => {
 				<span className={styles.title}>Duration</span>
 				<span className={styles.content}>{anime.duration}</span>
 			</div>
-		</motion.div>
+		</ListView>
 	);
 };
